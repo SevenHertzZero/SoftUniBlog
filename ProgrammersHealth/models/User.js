de956f837538669b0d15fdb1e956f19061801612
Metodi <metody91@abv.bg>
2017-04-24 12:23:03 +0300

    Set Up Node.js Express Project,creating user entity,create connection with MongoDB.

---
+++ ProgrammersHealth/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+let userSchema = mongoose.Schema(
+    {
+        email: {type: String, required: true, unique: true},
+        passwordHash: {type: String, required: true},
+        fullName: {type: String, required: true},
+        salt: {type: String, required: true}
+    }
+);
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
